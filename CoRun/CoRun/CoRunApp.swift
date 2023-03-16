e6a3a34dbeebe7fea233d241c850115c3f4b00cc
MRicharx <mricharx123@gmail.com>
2023-03-16 14:52:17 +0700

    Create Project

---
+++ CoRun/CoRun/CoRunApp.swift
@@ -0,0 +1,20 @@
+//
+//  CoRunApp.swift
+//  CoRun
+//
+//  Created by Marselus Richard on 16/03/23.
+//
+
+import SwiftUI
+
+@main
+struct CoRunApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
