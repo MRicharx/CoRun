bc285820a1b9b21df670bafc6a2b43952c7a187e
MRicharx <mricharx123@gmail.com>
2023-03-26 17:10:21 +0700

    Create QR Pop Up View and generate qr func

--- CoRun/CoRun/utils/tool/TQr.swift
+++ CoRun/CoRun/utils/tool/TQr.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import UIKit
+import CoreImage.CIFilterBuiltins
+
+///TQR -  QR Tool
+///Class cointain function to help handle QR
+class TQR{
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
+    func generateQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+
+        if let outputImage = filter.outputImage {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
+            }
+        }
+
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+
+}
--- CoRun/CoRun/view/profile/sub-view/qr-popup/ProfileQRPopUp.swift
+++ CoRun/CoRun/view/profile/sub-view/qr-popup/ProfileQRPopUp.swift
@@ -6,15 +6,90 @@
 //
 
 import SwiftUI
+import UniformTypeIdentifiers
 
 struct ProfileQRPopUp: View {
+    let qr = TQR()
+    
+    ///Define user coach id
+    @State var id:String
+    ///Define if id copied to clipboard
+    @State var showCopiedToast = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 24){
+            //MARK: QR Section
+            VStack(spacing:24){
+                Group{
+                    Text("Your Coach QR")
+                    
+                    Image(uiImage: qr.generateQRCode(from: id))
+                        .interpolation(.none)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxHeight:180)
+                    
+                    Text("Ask trainee to scan this QR Code")
+                }
+                .modifier(MFont.Headline(size:18))
+                    .modifier(MColor.Text())
+            }.padding(18)
+                .modifier(MView.Card())
+            
+            //MARK: Divider
+            HStack(spacing: 18){
+                CDivider(color: MColor.ColorPalette().textDisabled)
+                
+                Text("Or")
+                    .modifier(MFont.Headline(size:16))
+                    .modifier(MColor.DisabledText())
+                
+                CDivider(color: MColor.ColorPalette().textDisabled)
+            }.modifier(MView.safePadding())
+            
+            //MARK: Copy ID Section
+            VStack(spacing: 12){
+                Text("Your Coach ID: ")
+                    .modifier(MFont.Headline(size:18))
+                    .modifier(MColor.Text())
+                
+                HStack(spacing: 18){
+                    Group{
+                        Text(id)
+                        Button{
+                            //TODO: Copy ID to clipboard
+                            UIPasteboard.general.string = id
+                            showCopiedToast=true
+                        }label:{
+                            Image(systemName: "doc.on.clipboard.fill")
+                                .modifier(MColor.Primary())
+                        }
+                    }
+                    .modifier(MFont.Title(size:28))
+                }
+                
+                Text("Ask trainee to input this Code")
+                    .modifier(MFont.Headline(size:18))
+                    .modifier(MColor.Text())
+            }
+            .padding(24)
+            .modifier(MView.Card())
+        }
+        .alert(
+            "ID Copied",
+            isPresented: $showCopiedToast){
+                Button{
+                    //Do Nothing
+                }label:{
+                    Text("OK")
+                }
+            }
+        
     }
 }
 
 struct ProfileQRPopUp_Previews: PreviewProvider {
     static var previews: some View {
-        ProfileQRPopUp()
+        ProfileQRPopUp(id:"youtube.com")
     }
 }
