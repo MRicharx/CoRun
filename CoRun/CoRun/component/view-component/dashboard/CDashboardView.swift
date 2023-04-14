4efa5d55216c6d4432fb7a98079d61d73e9a26b3
MRicharx <mricharx123@gmail.com>
2023-04-14 20:15:15 +0700

    Fix: Adjust dashboard to new UI

--- CoRun/CoRun/component/view-component/dashboard/CDashboardView.swift
+++ CoRun/CoRun/component/view-component/dashboard/CDashboardView.swift
@@ -11,6 +11,8 @@ import SwiftUI
 ///Custom view for create and edit training session
 struct CDashboardView: View {
     ///Define distance target
+    @State var title = "Berlari"
+    ///Define distance target
     @State var targetDistance = ""
     ///Define pace target in minute
     @State var targetPaceMin = ""
@@ -20,52 +22,88 @@ struct CDashboardView: View {
     @State var targetIntensity = ""
     
     var body: some View {
-        VStack(spacing: 18){
-            //MARK: Date
-            VStack(alignment:.leading){
-                Text("Date")
-                    .modifier(MFont.Headline(size:16))
-                    .modifier(MColor.DisabledText())
-                Text("20 March 2023")
-                    .modifier(MFont.Headline())
-                    .modifier(MColor.Text())
-            }
-            .modifier(MView.FillToLeftFrame())
-            
-            //MARK: Target Distance
-            CTextfield(title: "Target Distance",hint:"--",unit: "km",type: .number, input: $targetDistance)
-            
-            HStack{
-                //MARK: Target Pace
-                CTextfield(title: "Target Distance",hint:"--",unit: "\'",type: .number, input: $targetPaceMin)
+        VStack(spacing:12){
+            VStack(spacing:24){
+                //MARK: Date
+                VStack(alignment:.leading){
+                    Text("Date")
+                        .modifier(MFont.Headline(size:16))
+                        .modifier(MColor.DisabledText())
+                    Text("20 March 2023")
+                        .modifier(MFont.Headline())
+                        .modifier(MColor.Text())
+                }
+                .modifier(MView.FillToLeftFrame())
                 
-                //MARK: Target Distance
-                CTextfield(title: "",hint:"--",unit: "\"",type: .number, input: $targetPaceSec)
+                CTextfield(title: "Activity Title", input: $title)
+                
+                HStack(spacing:24){
+                    CDivider()
+                    Text("Optional")
+                        .modifier(MFont.Headline(size:18))
+                        .modifier(MColor.DisabledText())
+                    CDivider()
+                }
+                
+                //MARK: Goal
+                VStack{
+                    //MARK: Target Distance
+                    CTextfield(title: "Target Distance",hint:"--",unit: "km",type: .number, input: $targetDistance)
+                    
+                    //MARK: Pace
+                    HStack(spacing:18){
+                        //MARK: Target Pace
+                        CTextfield(title: "Target Pace",hint:"--",unit: "\'",type: .number, input: $targetPaceMin)
+                        
+                        //MARK: Target Distance
+                        CTextfield(title: "",hint:"--",unit: "\"",type: .number, input: $targetPaceSec)
+                    }
+                    
+                    //MARK: Duration
+                    HStack(spacing:18){
+                        //MARK: Hour
+                        CTextfield(title: "Duration",hint:"--",unit: "hour",type: .number, input: $targetPaceMin)
+                        
+                        //MARK: minute
+                        CTextfield(title: "",hint:"--",unit: "min",type: .number, input: $targetPaceSec)
+                    }
+                    
+                    //MARK: Target Intensity
+                    CTextfield(title: "Target Intensity",hint:"--",unit: "%",type: .number, input: $targetIntensity)
+                    
+                    //MARK: Desc
+                    CTextfield(title: "Description",hint:"",unit: "",type: .number, input: $targetIntensity)
+                }
+                
             }
-            
-            //MARK: Target Intensity
-            CTextfield(title: "Target Intensity",hint:"--",unit: "%",type: .number, input: $targetIntensity)
+            .padding(24)
+            .modifier(MView.Card())
             
             //MARK: Button List
-            VStack(spacing: 18){
+            VStack(spacing: 0){
                 Button{
                     //TODO: Save Session Feature
                 }label: {
                     Text("Save Session")
                         .modifier(MFont.Headline(size:18))
-                }.buttonStyle(MButton.DefaultButton(isActive: true,padding: 12))
+                        .modifier(MColor.Primary())
+                        .modifier(MView.FillToLeftFrame())
+                }.buttonStyle(MButton.ListButton())
                 
                 Button{
                     //TODO: Discard Session Feature
                 }label: {
                     Text("Discard")
                         .modifier(MFont.Headline(size:18))
-                }.buttonStyle(MButton.DangerButton(isActive: true,invert: true,padding: 12))
+                        .modifier(MColor.Danger())
+                        .modifier(MView.FillToLeftFrame())
+
+                }.buttonStyle(MButton.ListButton())
                 
             }
+            
+            Spacer()
         }
-        .padding(24)
-        .modifier(MView.FillFrame())
     }
 }
 
